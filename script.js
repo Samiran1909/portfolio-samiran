a0bd3ff34318d3425fd8ae34dcbae0e66d3596dd
Samiran1909 <132904064+Samiran1909@users.noreply.github.com>
2025-04-10 14:45:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+document.getElementById('download-btn').addEventListener('click', () => {
+    document.getElementById('modal').style.display = 'block';
+});
+
+document.getElementById('yes-btn').addEventListener('click', () => {
+    window.open('Samiran Das MBA(A)02.pdf', '_blank');
+    document.getElementById('modal').style.display = 'none';
+});
+
+document.getElementById('no-btn').addEventListener('click', () => {
+    document.getElementById('modal').style.display = 'none';
+});
+
+document.getElementById('read-more').addEventListener('click', () => {
+    document.querySelector('.more-content').style.display = 'block';
+    document.querySelector('#read-more').style.display = 'none';
+});
+
+document.getElementById('read-less').addEventListener('click', () => {
+    document.querySelector('.more-content').style.display = 'none';
+    document.querySelector('#read-more').style.display = 'inline-block';
+})
